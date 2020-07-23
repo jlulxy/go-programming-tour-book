18b599d13b9d8ae77676c70e151a72f78de0fc4d
yvanli <lxy4200@163.com>
2020-07-23 21:01:19 +0800

    feat: add init config for server

---
+++ blog-service/global/setting.go
@@ -0,0 +1,9 @@
+package global
+
+import "github.com/jlulxy/go-programming-tour-book/blog-service/pkg/setting"
+
+var (
+	ServerSetting   *setting.ServerSettingS
+	AppSetting      *setting.AppSettingS
+	DataBaseSetting *setting.DatabaseSettingS
+)
--- blog-service/main.go
+++ blog-service/main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"time"
+
+	"github.com/jlulxy/go-programming-tour-book/blog-service/global"
+
+	"github.com/jlulxy/go-programming-tour-book/blog-service/pkg/setting"
 
 	"github.com/jlulxy/go-programming-tour-book/blog-service/internal/routers"
 )
 
+func init() {
+	err := setupSetting()
+	if err != nil {
+		log.Fatalf("init.setupSeting err :%v", err)
+	}
+}
 func main() {
 	router := routers.NewRouter()
 	s := &http.Server{
@@ -17,4 +28,26 @@ func main() {
 		MaxHeaderBytes: 1 << 20,
 	}
 	s.ListenAndServe()
+}
+
+func setupSetting() error {
+	setting, err := setting.NewSetting()
+	if err != nil {
+		return err
+	}
+	err = setting.ReadSection("Server", global.ServerSetting)
+	if err != nil {
+		return err
+	}
+	err = setting.ReadSection("App", global.AppSetting)
+	if err != nil {
+		return err
+	}
+	err = setting.ReadSection("DataBase", global.DataBaseSetting)
+	if err != nil {
+		return err
+	}
+	global.ServerSetting.ReadTimeout *= time.Second
+	global.ServerSetting.WriteTimeout *= time.Second
+	return nil
 }
